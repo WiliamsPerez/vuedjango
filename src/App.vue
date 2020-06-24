b532a18803fbca3fb1d117776fc4e34815c4c92f
Wiliams Perez <skrillexwili8@gmail.com>
2020-06-23 21:11:57 -0500

    Creacion de enlaces de categorias y tipos para el Header

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   <Header/> 
+   <Categories/>
+    <router-link to="/">List</router-link>
+    <router-link to="/detail">Detail</router-link>
+    
+     <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from './components/Header'
+import Categories from './components/Types'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Categories
+    
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
