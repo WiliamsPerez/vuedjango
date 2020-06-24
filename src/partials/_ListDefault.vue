634827ee5140dff7ab729c78011780ca3c38411b
Wiliams Perez <skrillexwili8@gmail.com>
2020-06-24 14:38:51 -0500

    mejoramiento de componente de carta para el listado de elementos

--- src/partials/_ListDefault.vue
+++ src/partials/_ListDefault.vue
@@ -1,18 +1,17 @@
 <template>
-    <div>
+    <div class="mt-3 mt-md-0">
         <div  v-for="e in elementsList" v-bind:key="e.id">
-            <router-link :to="'/detail/' + e.id " >
+            <router-link class="card-link" :to="'/detail/' + e.id " >
             <b-card
-            :title="e.title"
-            >
+            :header="e.title"
+            header-bg-variant="primary"
+            header-text-variant="white">
 
             <b-card-text>
                 {{ e.description }}
             </b-card-text>
-
             </b-card>
             </router-link>
-
         </div>
     </div>
 </template>
@@ -25,20 +24,22 @@ export default {
     ],
      data(){
          return{           
-             elementsLis: [ ]
+             elements: [ ]
          };
 
      },
-     methods: {
-        
-         
-     },
+     methods: {}
 }
 </script>
 <style>
-.box {
-    border: 3px solid #ccc;
-    margin: 5px 0 0 0;
+a.card-link, 
+a.card-link:hover {
+    color: #222;
+    text-decoration: none;
+}
+a.card-link:hover .card-body{
+    background: #f8f9fa;
+
 }
 
 </style>
