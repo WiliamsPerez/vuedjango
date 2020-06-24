ed36adc4947ae86c5f5ed618f6eaa220600879f3
Wiliams Perez <skrillexwili8@gmail.com>
2020-06-23 22:59:04 -0500

    componentes de listado de elementos

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import App from './App.vue'
 
 
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import VueRouter from 'vue-router'
 
 import List from './components/List'  
 import Detail from './components/Detail'
+import ListCategory from './components/ListCategory'
 
 Vue.use(VueRouter)
 Vue.use(BootstrapVue)
@@ -20,12 +21,14 @@ Vue.config.productionTip = false
 
 const routes =[
   {path:'/', component:List},
-  {path:'/detail/:id', component:Detail}
+  {path:'/detail/:id', component:Detail},
+  {path:'/category/:id/elements', component: ListCategory }
 ]
 
 const router = new VueRouter({
   mode: 'history',
-  routes // short for `routes: routes`
+  routes 
+  // short for `routes: routes`
 })
 
 new Vue({
